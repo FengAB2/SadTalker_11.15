b4e28709ba520e647ccffd320a4d0c207615c1f2
FengAB2 <115358776+FengAB2@users.noreply.github.com>
2023-11-22 23:33:21 +0800

    Update generate_list.py

--- src/face3d/util/generate_list.py
+++ src/face3d/util/generate_list.py
@@ -1,10 +1,15 @@
 """This script is to generate training list files for Deep3DFaceRecon_pytorch
+这段代码是为 Deep3DFaceRecon_pytorch 生成训练列表文件。
 """
 
 import os
 
 # save path to training data
 def write_list(lms_list, imgs_list, msks_list, mode='train',save_folder='datalist', save_name=''):
+    """接受三个列表，分别是 lms_list（包含关键点文件的路径列表）、
+    imgs_list（包含图像文件的路径列表）
+    和 msks_list（包含掩码文件的路径列表）。
+    """"
     save_path = os.path.join(save_folder, mode)
     if not os.path.isdir(save_path):
         os.makedirs(save_path)
@@ -19,6 +24,15 @@ def write_list(lms_list, imgs_list, msks_list, mode='train',save_folder='datalis
 
 # check if the path is valid
 def check_list(rlms_list, rimgs_list, rmsks_list):
+    """接受三个列表，分别是 rlms_list（原始关键点文件路径列表）、
+    rimgs_list（原始图像文件路径列表）和 
+    rmsks_list（原始掩码文件路径列表）。
+    调用 check_list 函数，可以筛选出有效的数据样本，并将这些有效样本的路径写入训练列表文件，方便在训练深度学习模型时使用。
+
+    对每个数据样本的路径进行检查，确保关键点、图像和掩码文件都存在。
+    如果存在，则将路径添加到新的列表 lms_list、imgs_list 和 msks_list 中，并输出一个标志 'true'；否则输出 'false'。
+    返回筛选后的列表。
+    """
     lms_list, imgs_list, msks_list = [], [], []
     for i in range(len(rlms_list)):
         flag = 'false'
