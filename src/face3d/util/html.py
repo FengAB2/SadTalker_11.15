b0fcc4f803b0b343511c479da8ea608f29144b91
FengAB2 <115358776+FengAB2@users.noreply.github.com>
2023-11-22 23:37:01 +0800

    Update html.py

--- src/face3d/util/html.py
+++ src/face3d/util/html.py
@@ -9,6 +9,8 @@ class HTML:
      It consists of functions such as <add_header> (add a text header to the HTML file),
      <add_images> (add a row of images to the HTML file), and <save> (save the HTML to the disk).
      It is based on Python library 'dominate', a Python library for creating and manipulating HTML documents using a DOM API.
+    这段代码定义了一个 HTML 类，该类用于生成一个包含图片和文本的 HTML 文件，以展示网页内容。
+    
     """
 
     def __init__(self, web_dir, title, refresh=0):
@@ -18,6 +20,8 @@ class HTML:
             web_dir (str) -- a directory that stores the webpage. HTML file will be created at <web_dir>/index.html; images will be saved at <web_dir/images/
             title (str)   -- the webpage name
             refresh (int) -- how often the website refresh itself; if 0; no refreshing
+            __init__ 方法接受三个参数，分别是 web_dir（存储网页的目录路径）、title（网页标题）、refresh（网页刷新间隔时间）。
+            如果指定了 refresh 大于 0，则在 HTML 文件的头部添加刷新的元信息。
         """
         self.title = title
         self.web_dir = web_dir
@@ -41,6 +45,7 @@ class HTML:
 
         Parameters:
             text (str) -- the header text
+            add_header 方法用于在 HTML 文件中插入标题。接受一个参数 text，表示要插入的标题文本。
         """
         with self.doc:
             h3(text)
@@ -52,6 +57,9 @@ class HTML:
             ims (str list)   -- a list of image paths
             txts (str list)  -- a list of image names shown on the website
             links (str list) --  a list of hyperref links; when you click an image, it will redirect you to a new page
+            add_images 方法用于在 HTML 文件中插入一行包含多张图片的表格。
+            接受四个参数，分别是 ims（图片路径列表）、txts（图片名称列表）、links（超链接列表）和 width（图片宽度）。
+            对于每一张图片，都在表格中的一个单元格内显示，带有超链接和指定宽度。
         """
         self.t = table(border=1, style="table-layout: fixed;")  # Insert a table
         self.doc.add(self.t)
@@ -66,7 +74,9 @@ class HTML:
                             p(txt)
 
     def save(self):
-        """save the current content to the HMTL file"""
+        """save the current content to the HMTL file
+        save 方法将当前 HTML 内容保存到指定的 HTML 文件中。
+        """
         html_file = '%s/index.html' % self.web_dir
         f = open(html_file, 'wt')
         f.write(self.doc.render())
@@ -74,6 +84,10 @@ class HTML:
 
 
 if __name__ == '__main__':  # we show an example usage here.
+    """
+    __main__ 部分，示例用法展示了如何创建一个 HTML 实例，添加标题和图片，并保存为 HTML 文件。
+    在这个示例中，生成了一个包含四张图片的网页，每张图片都带有超链接和名称。
+    """
     html = HTML('web/', 'test_html')
     html.add_header('hello world')
 
