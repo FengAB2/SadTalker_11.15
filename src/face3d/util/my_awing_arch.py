650a0ef98a01f1aa051ecc398cc5e59d1bbe6416
FengAB2 <115358776+FengAB2@users.noreply.github.com>
2023-11-23 21:24:22 +0800

    Update my_awing_arch.py

--- src/face3d/util/my_awing_arch.py
+++ src/face3d/util/my_awing_arch.py
@@ -128,6 +128,8 @@ class AddCoordsTh(nn.Module):
 class CoordConvTh(nn.Module):
     """CoordConv layer as in the paper.
     "CoordConv layer as in the paper" 指的是在论文中描述的 CoordConv（Coordinate Convolution）层的实现。
+    CoordConv 的主要思想是在卷积神经网络（CNN）的输入中引入额外的坐标信息，以帮助模型更好地理解输入数据的空间结构和位置关系。
+    这是通过在输入数据中添加表示位置信息的坐标通道来实现的。这种做法有助于解决标准 CNN 在处理位置信息时的一些困难，尤其是对于不规则的输入。
     """
 
     def __init__(self, x_dim, y_dim, with_r, with_boundary, in_channels, first_one=False, *args, **kwargs):
@@ -136,11 +138,13 @@ class CoordConvTh(nn.Module):
         in_channels += 2
         if with_r:
             in_channels += 1
-        if with_boundary and not first_one:
+        if with_boundary and not first_one:  #first_one表示这是不是模型中的第一个 CoordConv 层
             in_channels += 2
         self.conv = nn.Conv2d(in_channels=in_channels, *args, **kwargs)
 
     def forward(self, input_tensor, heatmap=None):
+        # 首先通过调用 AddCoordsTh 实例的 forward 方法，将坐标信息添加到输入张量中，得到增强后的张量 ret。
+        
         ret = self.addcoords(input_tensor, heatmap)
         last_channel = ret[:, -2:, :, :]
         ret = self.conv(ret)
@@ -149,6 +153,7 @@ class CoordConvTh(nn.Module):
 
 def conv3x3(in_planes, out_planes, strd=1, padding=1, bias=False, dilation=1):
     '3x3 convolution with padding'
+    # 创建一个 3x3 的卷积层
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=strd, padding=padding, bias=bias, dilation=dilation)
 
 
@@ -156,6 +161,13 @@ class BasicBlock(nn.Module):
     expansion = 1
 
     def __init__(self, inplanes, planes, stride=1, downsample=None):
+        """
+        __init__ 方法用于初始化 BasicBlock 类的实例。
+        inplanes: 输入通道数，即输入张量的通道数量。
+        planes: 输出通道数，即卷积操作后得到的特征图的通道数量。
+        stride: 步长，表示卷积核在输入张量上滑动的步幅，默认为 1。
+        downsample: 下采样，用于匹配输入和输出的维度，如果需要降低空间维度，可以提供一个下采样操作，默认为 None。
+        """
         super(BasicBlock, self).__init__()
         self.conv1 = conv3x3(inplanes, planes, stride)
         # self.bn1 = nn.BatchNorm2d(planes)
@@ -181,10 +193,11 @@ class BasicBlock(nn.Module):
 
         return out
 
-
+# 一个由卷积操作组成的块
 class ConvBlock(nn.Module):
-
+   
     def __init__(self, in_planes, out_planes):
+        
         super(ConvBlock, self).__init__()
         self.bn1 = nn.BatchNorm2d(in_planes)
         self.conv1 = conv3x3(in_planes, int(out_planes / 2))
@@ -192,7 +205,8 @@ class ConvBlock(nn.Module):
         self.conv2 = conv3x3(int(out_planes / 2), int(out_planes / 4), padding=1, dilation=1)
         self.bn3 = nn.BatchNorm2d(int(out_planes / 4))
         self.conv3 = conv3x3(int(out_planes / 4), int(out_planes / 4), padding=1, dilation=1)
-
+# 如果输入通道数 in_planes 不等于输出通道数 out_planes，则定义一个下采样分支。
+# 下采样分支使用 nn.Sequential 定义，包括批标准化层、ReLU 激活函数以及 1x1 卷积层，将输入通道数降至 out_planes。
         if in_planes != out_planes:
             self.downsample = nn.Sequential(
                 nn.BatchNorm2d(in_planes),
@@ -203,6 +217,18 @@ class ConvBlock(nn.Module):
             self.downsample = None
 
     def forward(self, x):
+        """
+        forward 方法定义了 ConvBlock 的前向传播逻辑。
+        首先将输入张量 x 赋值给 residual，作为残差连接的基准。
+        out1: 对输入进行批标准化、ReLU 激活函数和第一个卷积操作。
+        out2: 对 out1 进行批标准化、ReLU 激活函数和第二个卷积操作。
+        out3: 对 out2 进行批标准化、ReLU 激活函数和第三个卷积操作。
+        将 out1、out2 和 out3 沿着通道维度进行拼接，使用 torch.cat 函数。
+        如果存在下采样分支，对 residual 进行下采样。
+        将下采样结果与拼接后的特征图相加，实现了残差连接。
+        返回最终的输出结果。
+        
+        """
         residual = x
 
         out1 = self.bn1(x)
@@ -226,11 +252,20 @@ class ConvBlock(nn.Module):
 
         return out3
 
-
+# 这段代码定义了一个名为 HourGlass 的 PyTorch 模块，
+# 表示一个由多个堆叠的 Hourglass 模块组成的结构。Hourglass 模块通常用于图像处理任务，如关键点检测或姿态估计。
 class HourGlass(nn.Module):
 
     def __init__(self, num_modules, depth, num_features, first_one=False):
         super(HourGlass, self).__init__()
+        """
+        num_modules: Hourglass 模块的数量，表示网络中有多少个堆叠的 Hourglass 模块。
+        depth: Hourglass 模块的深度，表示每个 Hourglass 模块内部有多少层。
+        num_features: 特征通道数，表示每个 Hourglass 模块的特征通道数量。
+        first_one: 一个布尔值，表示是否是网络中的第一个 Hourglass 模块。
+        self.coordconv: 创建了一个名为 coordconv 的 CoordConvTh 模块，用于在 Hourglass 模块中添加坐标信息。这是一个在前面提到的 CoordConvTh 类的实例化。
+        self._generate_network(self.depth): 调用 _generate_network 方法来生成 Hourglass 模块内部的网络结构。
+        """
         self.num_modules = num_modules
         self.depth = depth
         self.features = num_features
@@ -248,6 +283,12 @@ class HourGlass(nn.Module):
         self._generate_network(self.depth)
 
     def _generate_network(self, level):
+        """
+        _generate_network 方法用于在 Hourglass 模块内部生成指定深度的网络结构。
+        创建了多个名为 'b1_' + str(level)、'b2_' + str(level)、'b2_plus_' + str(level) 和 'b3_' + str(level) 的 ConvBlock 模块，
+        构建了 Hourglass 模块的内部结构。这些模块可能包含卷积层、批标准化层等操作。
+        递归地调用 _generate_network 方法，构建 Hourglass 模块的金字塔状结构。
+        """
         self.add_module('b1_' + str(level), ConvBlock(256, 256))
 
         self.add_module('b2_' + str(level), ConvBlock(256, 256))
@@ -260,6 +301,9 @@ class HourGlass(nn.Module):
         self.add_module('b3_' + str(level), ConvBlock(256, 256))
 
     def _forward(self, level, inp):
+        """
+        _forward 方法实现了 Hourglass 模块的前向传播逻辑。
+        """
         # Upper branch
         up1 = inp
         up1 = self._modules['b1_' + str(level)](up1)
@@ -282,13 +326,25 @@ class HourGlass(nn.Module):
         return up1 + up2
 
     def forward(self, x, heatmap):
+        """
+        接受输入张量 x 和热图 heatmap。
+        调用 coordconv 模块，将坐标信息添加到输入张量中。
+        调用 _forward 方法进行 Hourglass 模块的前向传播，返回 Hourglass 模块的输出和热图中的最后两个通道。
+        """
         x, last_channel = self.coordconv(x, heatmap)
         return self._forward(self.depth, x), last_channel
 
 
+# 它是一个具有多个 Hourglass 模块的网络，用于进行人脸关键点检测。
 class FAN(nn.Module):
 
     def __init__(self, num_modules=1, end_relu=False, gray_scale=False, num_landmarks=68, device='cuda'):
+        """
+        初始化模型的参数，如 num_modules 表示 Hourglass 模块的数量，end_relu 表示最终输出是否应用 ReLU 激活，gray_scale 表示输入是否为灰度图像，
+        num_landmarks 表示要检测的关键点数量，device 表示运行模型的设备。
+        创建了模型的基础部分，包括一些卷积块 (CoordConvTh, ConvBlock) 和 Hourglass 模块。
+        通过 add_module 方法将这些子模块添加到模型中。
+        """
         super(FAN, self).__init__()
         self.device = device
         self.num_modules = num_modules
@@ -342,6 +398,13 @@ class FAN(nn.Module):
                                 nn.Conv2d(num_landmarks + 1, 256, kernel_size=1, stride=1, padding=0))
 
     def forward(self, x):
+        """
+        定义了模型的前向传播逻辑。
+        输入首先通过卷积块和池化层进行处理。
+        接着通过多个 Hourglass 模块进行处理，每个模块都生成一组关键点的预测（tmp_out）。
+        输出包含了每个 Hourglass 模块生成的关键点预测，以及用于边界信息的通道（boundary_channels）。
+        边界信息通常指示关键点位置的边缘或边界。
+        """
         x, _ = self.conv1(x)
         x = F.relu(self.bn1(x), True)
         # x = F.relu(self.bn1(self.conv1(x)), True)
@@ -377,6 +440,11 @@ class FAN(nn.Module):
         return outputs, boundary_channels
 
     def get_landmarks(self, img):
+        """
+        该方法接受一张图像，将其预处理成模型可以接受的格式，并调用模型的前向传播方法获取关键点的预测。
+        预测的关键点坐标通过 calculate_points 函数计算，然后根据图像的原始尺寸进行调整。
+        返回关键点的预测坐标。
+        """
         H, W, _ = img.shape
         offset = W / 64, H / 64, 0, 0
 
